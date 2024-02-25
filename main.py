e0cdebf105deafc3fdb79baa5d0da04d75554277
bartekw91 <161207607+bartekw91@users.noreply.github.com>
2024-02-25 13:03:22 +0100

    Dodanie pliku plik1

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+print(sys.version) # Pokaż wersję systemu
+
+# git config --list - pokaż konfigurację . User name i user email musi się pokazać.
+# git config --global user.name "bartekw91" - Dodaj nazwę użytkownika
+# git config --global user.email "161207607+bartekw91@users.noreply.github.com" - Dodaj e-mail (w tym przypadku GitHuba)
+
+a = 'General Kenobi'
+print(a)
